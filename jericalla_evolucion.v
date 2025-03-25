c8d0bf7bc652568a064dda4341a2894e823e04cf
MelanieLopez777 <melanie.lopez7404@alumnos.udg.mx>
2025-03-24 19:38:02 -0600

    Resultados obtenidos a partir del uso de los valores almacenados en 'datos.txt'

--- jericalla_evolucion.v
+++ jericalla_evolucion.v
@@ -33,7 +33,6 @@ wire        wire_buffer_1_uc_e_write_ram;
 wire        wire_buffer_1_uc_demux;
 wire [3:0]  wire_buffer_1_uc_alu_opcode; 
 wire        wire_buffer_1_uc_e_write_br;
-//wire [4:0]  wire_buffer_1_wA;
 wire [31:0] wire_buffer_1_data_register_1;
 wire [31:0] wire_buffer_1_data_register_2;
 
@@ -42,13 +41,8 @@ wire        wire_buffer_2_uc_e_read_ram;
 wire        wire_buffer_2_uc_e_write_ram;
 wire        wire_buffer_2_uc_e_write_br;
 wire [31:0] wire_buffer_2_address_ram;
-//wire [4:0]  wire_buffer_2_wA;
-wire [31:0] wire_buffer_2_dW;
 wire [31:0] wire_buffer_2_din_ram;
 
-/* Cables Buffer 3   <-----
-wire wire_buffer_3_uc_e_write_br;
-*/
 
 // Cables Demultiplexor
 wire [31:0] wire_demux_data_ram;
@@ -82,9 +76,9 @@ contador contador_inst(
 banco_de_registros banco_de_registros_inst(
     .read_address_1(instruction[9:5]),
     .read_address_2(instruction[4:0]),
-    .write_address(instruction[14:10]),  //<-- Se modifico para sincronizacion
-    .data_write(wire_buffer_2_dW),
-    .write_enable(wire_demux2_dout), //<-- Cambie el cable que envía la señal
+    .write_address(instruction[14:10]), 
+    .data_write(wire_alu_result),
+    .write_enable(wire_demux2_dout), 
     .data_register_1(wire_rb_data_register_1),
     .data_register_2(wire_rb_data_register_2)
 );
@@ -108,14 +102,6 @@ buffer1 buffer1_inst(
 	.o_DR1(wire_buffer_1_data_register_1),
 	.o_DR2(wire_buffer_1_data_register_2)
 );
-
-/*buffer3 buffer3_inst( //<-- Añadí la instancia del buffer 3
-	
-	.clk(clk),
-	.i_uc_e_write_br(wire_buffer_2_uc_e_write_br),
-	.dout_buffer3(wire_buffer_3_uc_e_write_br)
-);*/
-
 
 demultiplexor demultiplexor_inst(			
     .selector(wire_buffer_1_uc_demux),
@@ -145,16 +131,12 @@ buffer2 buffer2_inst
 	.i_uc_e_write_ram(wire_buffer_1_uc_e_write_ram),
 	.i_uc_e_write_br(wire_buffer_1_uc_e_write_br),
 	.i_result_demux(wire_demux_data_ram),
-	//.i_wA(wire_buffer_1_wA),
-	.i_alu_result(wire_alu_result),
 	.i_DR2(wire_buffer_1_data_register_2),
 	.clk(clk),
 	.o_uc_e_read_ram(wire_buffer_2_uc_e_read_ram),
 	.o_uc_e_write_ram(wire_buffer_2_uc_e_write_ram),
 	.o_uc_e_write_br(wire_buffer_2_uc_e_write_br),
 	.o_address_ram(wire_buffer_2_address_ram),
-	//.o_wA(wire_buffer_2_wA),
-	.o_dW(wire_buffer_2_dW),
 	.o_din_ram(wire_buffer_2_din_ram)
 );
 
